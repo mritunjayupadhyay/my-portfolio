852ffa04fca5b237755a22b7943646a9c6752896
Mritunjay <mupadhyay00@gmail.com>
2023-12-15 20:47:58 +0700

    🔧 chore(page.tsx): refactor code to improve readability and maintainability 🎨 style(page.tsx): format code and add missing imports for better organization 🐛 fix(page.tsx): fix button click handler to log the correct element ✨ feat(page.tsx): add animation and styling to the greeting hand emoji ✨ feat(page.tsx): add introduction section with name, description, and contact information

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,46 @@
-import Image from 'next/image'
+"use client";
+import { pointright } from '@/assets/icons';
+import Link from 'next/link';
+import { useRef } from 'react';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      my portfolio
-    </main>
-  )
+  const hand = useRef(null);
+
+  function handleClick() {
+    console.log("animate this one", hand.current)
+  }
+    return (
+        <main className="w-full h-screen relative max-container">
+            <section className="intro">
+                <h1 className="head-text-light">
+                    Hello!
+                    <button onClick={handleClick} ref={hand} className="pl-4 animate">
+                    👋
+                    </button>
+                </h1>
+
+                <h2 className="head-text-light max-w-screen-sm">
+                    I'm
+                    <span className="pl-2 blue-gradient_text font-semibold drop-shadow">Mritunjay Upadhyay</span>, 
+                    a full stack engineer dedicated to developing and optimizing interactive, 
+                    user-friendly, and feature-rich applications.
+                    <span className="emoji technologist"></span>
+                </h2>
+
+                <h3 className="head-text-contact flex flex-wrap">
+                    <span className='pr-5'>Get in touch </span>
+                    <img
+                      src={pointright}
+                      alt={"pointright"}
+                      className='w-7 object-contain mr-4'
+                    />
+                    <span className=''>
+                    <Link href='/contact' className='blue-gradient_shadow'>
+                      mupadhyay00@gmail.com
+                    </Link>
+                    </span>
+                </h3>
+            </section>
+        </main>
+    )
 }
